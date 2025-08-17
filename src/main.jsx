106bb400fe865a436785530c093dfc1d449b898e
AxelTWC <30543699+AxelTWC@users.noreply.github.com>
2025-08-17 10:01:55 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,6 @@ import { Button } from "@/components/ui/button";
 import { Github, Linkedin, Mail } from "lucide-react";
 
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Portfolio from "./Portfolio";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Portfolio />
-  </React.StrictMode>
-);
-
 export default function Portfolio() {
   const projects = [
     {
